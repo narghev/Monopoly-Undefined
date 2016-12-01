67fe2c24e89fe0accbc00bf6cc24eed1b1be8df7
narghev <narghev1998@gmail.com>
2016-12-02 01:29:07 +0400

    Add Field Types & Map

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Monopoly-Undefined
-fork the Repo (from github)
-$git clone <your repo url>
+
+fork the Repo (from github);
+
+$git clone <your repo url>;
+$git remote add upstream https://github.com/narghev/Monopoly-Undefined;
+
 work on the files
-Write in the FB group to make sure whether something has been changed in the origin repository.
-$git remote add upstream https://github.com/narghev/Monopoly-Undefined
-$git fetch https://github.com/narghev/Monopoly-Undefined
-$git rebase upstream
+
+Write in the FB group to make sure whether something has been changed in the origin repository.;
+
+$git fetch https://github.com/narghev/Monopoly-Undefined;
+$git rebase upstream;
 $git push -u origin master
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
@@ -13,8 +15,98 @@ class Player {
     this.money = 1500;
     this.figure = "public/figures/fig"+players.length+".png";
     this.shops = [];
+    this.currentField = 0;
+  }
+  move(fields){
+    this.currentField = (40+this.currentField + fields)%40;
   }
 }
+
+class RegularStreet {
+  // type 0;
+
+  /*constructor(price){
+    this.owner = null;
+    this.price = price;
+  }*/
+}
+
+class Chance {
+  //type 4;
+}
+
+class TrainStation {
+  //type 3;
+}
+
+class IncomeTax {
+  //type 2;
+}
+
+class LuxuryTax {
+  //type 11;
+}
+
+class Water {
+  //type 9;
+}
+
+class Electricity {
+  //type 6;
+}
+
+class Chest {
+  //type 1;
+}
+
+class Jail {
+  //type 5;
+}
+
+class GoToJail {
+  //type 10;
+}
+
+const map = [{type: 8},
+{type: 0, fieldData: new RegularStreet()},
+{type: 1, fieldData: new Chest()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 2, fieldData: new IncomeTax()},
+{type: 3, fieldData: new TrainStation()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 4, fieldData: new Chance()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 5, fieldData: new Jail()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 6, fieldData: new Electricity()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 3, fieldData: new TrainStation()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 1, fieldData: new Chest()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 7},
+{type: 0, fieldData: new RegularStreet()},
+{type: 4, fieldData: new Chance()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 3, fieldData: new TrainStation()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 9, fieldData: new Water()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 10, fieldData: new GoToJail()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 1, fieldData: new Chest()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 3, fieldData: new TrainStation()},
+{type: 4, fieldData: new Chance()},
+{type: 0, fieldData: new RegularStreet()},
+{type: 11, fieldData: new LuxuryTax()},
+{type: 0, fieldData: new RegularStreet()}];
 
 const gameStart = ()=>{
   console.log("4 players are ready, the games has started.");
