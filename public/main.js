c8fe891b8a608546c370c97b40349f22acf7640e
narghev <narghev1998@gmail.com>
2016-12-03 13:30:41 +0400

    Chance and Chest cards working

--- public/main.js
+++ public/main.js
@@ -6,27 +6,31 @@ socket.on("playerImages", (imgs)=>{
 socket.on("playerInfoUpdate", (playerData)=>{
   let money = JSON.parse(playerData).socketMoney; //has to be in the player information, top left corner, REACT
   let property = JSON.parse(playerData).socketProperty; //has to be in the player information, top left corner, REACT
-});
-socket.on("mapInfoPlayerPos", (mapData)=>{
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  console.log(mapData);
-  for (let i=0; i<mapData.length; i++)
-    playerPositionUpdate(mapData[i], playerImgs[i]);
-  /*
-    An array, mapData[0] is the player1 position, mapData[1] is the player2 position,
-    mapData[2] is the player3 position, mapData[3] is the player4 position.
-    Give these values to the function wich is going to place the figures in the right position on the canvas.
-    for (let i of mapData)
-      yourFunction(i);
-  */
+  //let cards = JSON.parse(playerData.socketCards); //array, length = 2, boolean values, cards[0]= (true if player has a jail card) cards[1]= (true if player doesnt have to pay rent for the next house he moves to)
+  console.log(money, property);
 });
 socket.on("diceResults", (dice1, dice2)=>{
   //dice animation
 });
-//socket.emit('moveTheFigure');
+socket.on("getTheCard", (text)=>{
+  console.log(text);
+});
 
 window.onload = ()=>{
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
-
+  socket.on("mapInfoPlayerPos", (mapData)=>{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(mapData);
+    //for (let i=0; i<mapData.length; i++)
+      //playerPositionUpdate(mapData[i], playerImgs[i]);
+    /*
+      An array, mapData[0] is the player1 position, mapData[1] is the player2 position,
+      mapData[2] is the player3 position, mapData[3] is the player4 position.
+      Give these values to the function wich is going to place the figures in the right position on the canvas.
+      for (let i of mapData)
+        yourFunction(i);
+    */
+  });
+    //socket.emit('moveTheFigure');
 }
--- server.js
+++ server.js
@@ -15,8 +15,12 @@ class Player {
     this.money = 1500;
     this.figure = "public/figures/fig"+players.length+".png";
     this.property = [];
+    this.hotelN = 0;
+    this.houseN = 0;
     this.currentField = 0;
     this.inJail = false;
+    this.noRentCard = false;
+    this.freeFromJailCard = false;
   }
   move(fields){
     this.currentField = (40+this.currentField + fields)%40;
@@ -47,27 +51,77 @@ class RegularStreet {
   }
 }
 
-const chanceArr = [{text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}},
-  {text: "", func(){}}];
+const chanceArr = [{text: "They think they can kick you (Go forward 3 spaces).", func(player){
+  player.move(3);
+  updateMapInfoPlayerPos();
+}},
+  {text: "You have no money (They have nothing to take you are finally free) – no rent.", func(player){
+    player.noRentCard = true;
+    updatePlayerInfo(player);
+  }},
+  {text: "Go to jail - Go directly to jail.", func(player){
+    player.inJail = true;
+    player.currentField = 10;
+    updateMapInfoPlayerPos();
+  }},
+  {text: "Go back 3 spaces.", func(player){
+    player.move(-3);
+    updateMapInfoPlayerPos();
+  }},
+  {text: "You have been elected Chairman of board (Pay each player 30$).", func(player){
+    player.money = player.money - (players.length-1)*30;
+    for (let i of players){
+      if (i!=player)
+        i.money += 30;
+      updatePlayerInfo(i);
+    }
+  }},
+  {text: "Make general repairs on all your property (Pay 15$ per house, 60$ per hotel).", func(player){
+    player.money = player.money - (player.houseN*15 + player.hotelN*60);
+    updatePlayerInfo(player);
+  }},
+  {text: "Pay poor tax of 15$.", func(player){
+    player.money -= 15;
+    updatePlayerInfo(player);
+  }},
+  {text: "Your stock stands low (For each property card you pay 10$).", func(player){
+    player.money -= player.property.length * 10;
+    updatePlayerInfo(player);
+  }},
+  {text: "Take a walk on the 5-th avenue (Don’t pay rent).", func(player){
+    player.currentField = 39;
+    updateMapInfoPlayerPos();
+  }},
+  {text: "Get out of jail, free (This card may be kept until needed).", func(player){
+    player.freeFromJailCard = true;
+    updatePlayerInfo(player);
+  }},
+  {text: "Bank pays you dividend of 50$.", func(player){
+    player.money += 50;
+    updatePlayerInfo(player);
+  }},
+  {text: "Advance to “GO” (Collect 200$).", func(player){
+    player.currentField = 0;
+    player.money += 200;
+    updatePlayerInfo(player);
+    updateMapInfoPlayerPos();
+  }},
+  {text: "Your building and loan matures (Collect 50$).", func(player){
+    player.money += 50;
+    updatePlayerInfo(player);
+  }},
+  {text: "Pay for telephone conversation 25$.", func(player){
+    player.money-=25;
+    updatePlayerInfo(player);
+  }}];
 
 class Chance {
   //type 4;
 
-  showChaceCard(){
+  show(player){
     let chosenCard = chanceArr[Math.floor(Math.random()*chanceArr.length)];
+    io.to(player.socketId).emit("getTheCard", chosenCard.text);
+    chosenCard.func(player);
   }
 
 }
@@ -124,8 +178,75 @@ class Electricity {
   */
 }
 
+const chestArr = [{text: "Income tax refund (Collect 20$).", func(player){
+  player.money += 20;
+  updatePlayerInfo(player);
+}},
+  {text: "Bank error in your favor (Collect 200$).", func(player){
+    player.money += 200;
+    updatePlayerInfo(player);
+  }},
+  {text: "From sale of stock you get 40$.", func(player){
+    player.money += 40;
+    updatePlayerInfo(player);
+  }},
+  {text: "Life insurance matures (Collect 100$).", func(){
+    player.money += 100;
+    updatePlayerInfo(player);
+  }},
+  {text: "Collect 50$ from every player.", func(player){
+    player.money = player.money + (players.length-1)*50;
+    for (let i of players){
+      if (i!=player)
+        i.money -= 50;
+      updatePlayerInfo(i);
+    }
+  }},
+  {text: "Relieve your own stress collect 20$.", func(player){
+    player.money += 20;
+    updatePlayerInfo(player);
+  }},
+  {text: "Pay hospital 100$.", func(player){
+    player.money -= 100;
+    updatePlayerInfo(player);
+  }},
+  {text: "Doctor’s fee pay 50$.", func(player){
+    player.money -= 50;
+    updatePlayerInfo(player);
+  }},
+  {text: "Pay school tax of 150$.", func(player){
+    player.money -= 150;
+    updatePlayerInfo(player);
+  }},
+  {text: "X-mas fund matures (Collect 45$).", func(player){
+    player.money += 45;
+    updatePlayerInfo(player);
+  }},
+  {text: "You have won second prize in a beauty contest (Collect 50$).", func(player){
+    player.money += 50;
+    updatePlayerInfo(player);
+  }},
+  {text: "From the charitable alliance (Collect 60$).", func(player){
+    player.money += 60;
+    updatePlayerInfo(player);
+  }},
+  {text: "Receive for services 25$.", func(player){
+    player.money += 60;
+    updatePlayerInfo(player);
+  }},
+  {text: "Go to jail- Go directly to jail.", func(player){
+    player.inJail = true;
+    player.currentField = 10;
+    updateMapInfoPlayerPos();
+  }}];
+
 class Chest {
   //type 1;
+  show(player){
+    let chosenCard = chestArr[Math.floor(Math.random()*chestArr.length)];
+    io.to(player.socketId).emit("getTheCard", chosenCard.text);
+    chosenCard.func(player);
+  }
 }
 
 class Jail {
@@ -172,14 +293,17 @@ const trainStationArr = [{type: 3, fieldData: new TrainStation()},
 const utilitiesArr = [{type: 6, fieldData: new Electricity()},
   {type: 9, fieldData: new Water()}];
 
+const chance = new Chance();
+const chest = new Chest();
+
 const map = [{type: 8},
 regStrT1Arr[0],
-{type: 1, fieldData: new Chest()},
+{type: 1},
 regStrT1Arr[1],
 {type: 2, fieldData: new IncomeTax()},
 trainStationArr[0],
 regStrT2Arr[0],
-{type: 4, fieldData: new Chance()},
+{type: 4},
 regStrT2Arr[1],
 regStrT2Arr[2],
 {type: 5, fieldData: new Jail()},
@@ -189,12 +313,12 @@ regStrT3Arr[1],
 regStrT3Arr[2],
 trainStationArr[1],
 regStrT4Arr[0],
-{type: 1, fieldData: new Chest()},
+{type: 1},
 regStrT4Arr[1],
 regStrT4Arr[2],
 {type: 7},
 regStrT5Arr[0],
-{type: 4, fieldData: new Chance()},
+{type: 4},
 regStrT5Arr[1],
 regStrT5Arr[2],
 trainStationArr[2],
@@ -205,16 +329,16 @@ regStrT6Arr[2],
 {type: 10, fieldData: new GoToJail()},
 regStrT7Arr[0],
 regStrT7Arr[1],
-{type: 1, fieldData: new Chest()},
+{type: 1},
 regStrT7Arr[2],
 trainStationArr[3],
-{type: 4, fieldData: new Chance()},
+{type: 4},
 regStrT8Arr[0],
 {type: 11, fieldData: new LuxuryTax()},
 regStrT8Arr[1]];
 
 const updatePlayerInfo = (player)=>{
-    io.to(player.socketId).emit('playerInfoUpdate', JSON.stringify({socketMoney: player.money, socketProperty: player.property}));
+    io.to(player.socketId).emit('playerInfoUpdate', JSON.stringify({socketMoney: player.money, socketProperty: player.property, socketCards: [player.freeFromJailCard, player.noRentCard]}));
 }
 
 const updateMapInfoPlayerPos = ()=>{
@@ -264,6 +388,9 @@ io.on('connection', (socket)=>{
     io.sockets.emit("diceResults", dice1, dice2);
     players[who(socket.id)].move(dice1+dice2);
     updateMapInfoPlayerPos();
+    if (map[players[who(socket.id)].currentField].type===4)
+      chance.show(players[who(socket.id)]);
+    else if (map[players[who(socket.id)].currentField].type===1)
+      chest.show(players[who(socket.id)]);
   });
-
 });
