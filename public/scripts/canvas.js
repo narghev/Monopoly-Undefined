1693a3aec021b94f9a1a90f8e0495d4ef796435e
secpsycho <zaven.snkhchyan@gmail.com>
2016-12-09 02:42:01 +0400

    canvas upgrade

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -3,34 +3,34 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const playerPositionUpdate = (canvas, fieldN, playerImg)=>{
-  let x = 631 ,y = 650;
+  let x = 838.3285714285714466 ,y = 863.57142857142859;
   if (fieldN > 0 && fieldN < 10){
-    y = 650;
-    x = 609-(58*fieldN)+5;
+y = 863.57142857142859;
+x = 809.1000000000000174-(77.0571428571428588*fieldN)+6.642857142857143;
   }
   else if (fieldN === 10){
-    y = 650;
-    x = 15;
+y = 863.57142857142859;
+x = 19.928571428571429;
   }
   else if (fieldN > 10 && fieldN < 20){
-    x = 15;
-    y = 609-(58*(fieldN%10))+5;
+x = 19.928571428571429;
+y = 809.1000000000000174-(77.0571428571428588*(fieldN%10))+6.642857142857143;
   }
   else if (fieldN === 20){
-    x=15;
-    y=15;
+x=19.928571428571429;
+y=19.928571428571429;
   }
   else if (fieldN > 20 && fieldN < 30){
-    y=15;
-    x=37+(58*(fieldN%10));
+y=19.928571428571429;
+x=49.1571428571428582+(77.0571428571428588*(fieldN%10));
   }
   else if (fieldN === 30){
-    y=15;
-    x=631;
+y=19.928571428571429;
+x=838.3285714285714466;
   }
   else if (fieldN > 30 && fieldN < 40){
-    x=631;
-    y=37+(58*(fieldN%10));
+x=838.3285714285714466;
+y=49.1571428571428582+(77.0571428571428588*(fieldN%10));
   }
-  ctx.drawImage(playerImg, x, y, 50, 50);
+  ctx.drawImage(playerImg, x, y, 50,50 );
 };
